cee29efad4761fa36b6a7a62f4aa76088a6c4048
Syauqi <syauqifrizman@gmail.com>
2024-08-14 10:55:23 +0700

    fix: Fix ChatScreen component to use a single state for messages

--- src/pages/archive/ChatScreen.jsx
+++ src/pages/archive/ChatScreen.jsx
@@ -1,5 +1,5 @@
 import { UserWithBubbleChat } from "@/components/UserWithBubbleChat";
-import { HeaderChatSection } from "../components/HeaderChatSection";
+import { HeaderChatSection } from "../../components/HeaderChatSection";
 import { TextareaWithButtonIcon } from "@/components/TextareaWithButtonIcon";
 import { SendIcon } from "lucide-react";
 import { AssistantWithBubbleChat } from "@/components/AssistantWithBubbleChat";
@@ -10,12 +10,21 @@ import { CardItemQuestionFaq } from "@/components/CardItemQuestionFaq";
 import { Skeleton } from "@/components/ui/skeleton";
 
 export function ChatScreen({ chatSessionID }) {
-  // ====================================================================================================
-  // state untuk chat
-  const [newMessage, setNewMessage] = useState({});
-  const [messages, setMessages] = useState([]);
+  // management pesan baru dari asisten
+  const [newAssistantMessage, setNewAssistantMessage] = useState({
+    fullname: "",
+    message: "",
+  });
+  // array (kumpulan) pesan dari asisten
+  const [assistantMessages, setAssistantMessages] = useState([]);
 
-  // ====================================================================================================
+  // management pesan baru dari user
+  const [newUserMessage, setNewUserMessage] = useState({
+    fullname: "",
+    message: "",
+  });
+  // array (kumpulan) pesan dari user
+  const [userMessages, setUserMessages] = useState([]);
 
   // state untuk menyimpan error dari API
   const [error, setError] = useState(null);
@@ -28,13 +37,18 @@ export function ChatScreen({ chatSessionID }) {
 
   // set pesan pertama dari asisten ketika halaman pertama kali di-load
   useEffect(() => {
+    // setNewAssistantMessage({
+    //   fullname: "Telkom Indonesia",
+    //   message: "Hai! Ada yang bisa saya bantu?",
+    // });
+
     async function getFirstChat() {
       setIsLoading(true);
       try {
         const responseFirstChat = await axios.get(
           `http://localhost:8000/api/chats/${chatSessionID}`
         );
-        setNewMessage(responseFirstChat.data.data.chat);
+        setNewAssistantMessage(responseFirstChat.data.data.chat);
       } catch (error) {
         setError(error.response.data.message);
       }
@@ -58,15 +72,26 @@ export function ChatScreen({ chatSessionID }) {
 
   // ketika ada pesan baru dari asisten, tambahkan ke array pesan asisten
   useEffect(() => {
-    if (newMessage.fullname && newMessage.message) {
-      setMessages((prevmessage) => [...prevmessage, newMessage]);
+    if (newAssistantMessage.fullname && newAssistantMessage.message) {
+      setAssistantMessages((prevmessage) => [
+        ...prevmessage,
+        newAssistantMessage,
+      ]);
     }
-  }, [newMessage]);
+  }, [newAssistantMessage]);
+
+  // ketika ada pesan baru dari user, tambahkan ke array pesan user
+  useEffect(() => {
+    if (newUserMessage.fullname && newUserMessage.message) {
+      setUserMessages((prevmessage) => [...prevmessage, newUserMessage]);
+      console.log(newUserMessage);
+    }
+  }, [newUserMessage]);
 
   // handle faq template
   function handleFaqButton(faq) {
     // add faq to user messages
-    setMessages((prevmessage) => [
+    setUserMessages((prevmessage) => [
       ...prevmessage,
       { fullname: "Anda", message: faq.question },
     ]);
@@ -91,7 +116,7 @@ export function ChatScreen({ chatSessionID }) {
       );
 
       // set pesan balasan dari asisten
-      setNewMessage(responseAssistant.data.data.chat);
+      setNewAssistantMessage(responseAssistant.data.data.chat);
     } catch (error) {
       setError(error);
     }
@@ -99,7 +124,7 @@ export function ChatScreen({ chatSessionID }) {
 
   // fungsi untuk meng-handle submit pesan dari user (passing function ke TextareaWithButtonIcon)
   function handleMessageSubmit(value) {
-    setNewMessage({
+    setNewUserMessage({
       fullname: value.fullname,
       message: value.message,
     });
@@ -125,6 +150,15 @@ export function ChatScreen({ chatSessionID }) {
           <HeaderChatSection />
         </div>
         <div className="flex-auto">
+          {assistantMessages
+            .filter((value) => {
+              return !(value.fullname === "" && value.message === "");
+            })
+            .map((value, index) => (
+              <AssistantWithBubbleChat key={index}>
+                <BubbleChat fullname={value.fullname} message={value.message} />
+              </AssistantWithBubbleChat>
+            ))}
           {isLoading ? (
             generateSkeleton()
           ) : (
@@ -141,30 +175,11 @@ export function ChatScreen({ chatSessionID }) {
               })}
             </div>
           )}
-          {messages
-            .filter((value) => {
-              return !(value.fullname === "" && value.message === "");
-            })
-            .map((value, index) => {
-              if (value.fullname === "Anda") {
-                return (
-                  <UserWithBubbleChat key={index}>
-                    <BubbleChat
-                      fullname={value.fullname}
-                      message={value.message}
-                    />
-                  </UserWithBubbleChat>
-                );
-              }
-              return (
-                <AssistantWithBubbleChat key={index}>
-                  <BubbleChat
-                    fullname={value.fullname}
-                    message={value.message}
-                  />
-                </AssistantWithBubbleChat>
-              );
-            })}
+          {userMessages.map((value, index) => (
+            <UserWithBubbleChat key={index}>
+              <BubbleChat fullname={value.fullname} message={value.message} />
+            </UserWithBubbleChat>
+          ))}
         </div>
         <div className="sticky bottom-0 bg-white">
           <TextareaWithButtonIcon
