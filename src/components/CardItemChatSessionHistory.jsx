9c705afa34297f48de06fc35534c3d9c53040cbc
Syauqi <syauqifrizman@gmail.com>
2024-08-14 14:19:48 +0700

    feat: Add CardItemChatSessionHistory component to change dropdown into card view

---
+++ src/components/CardItemChatSessionHistory.jsx
@@ -0,0 +1,24 @@
+import { LogInIcon } from "lucide-react";
+import { Button } from "./ui/button";
+
+export function CardItemChatSessionHistory({
+  chatSession,
+  setSelectedChatSession,
+}) {
+  return (
+    <div className="flex flex-row items-center justify-between p-4 bg-white shadow-md rounded-lg mb-2 w-64">
+      <div className="flex flex-col">
+        <h1 className="text-lg font-semibold">{chatSession.title}</h1>
+        <p className="text-sm text-gray-500">{chatSession.created_at}</p>
+      </div>
+      <div>
+        <Button
+          variant="secondary"
+          onClick={() => setSelectedChatSession(chatSession)}
+        >
+          <LogInIcon size="16" className="mr-2" /> Join
+        </Button>
+      </div>
+    </div>
+  );
+}
