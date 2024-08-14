d594fbdff69098431d09982d7639e4e3aa511ea8
Syauqi <syauqifrizman@gmail.com>
2024-08-14 12:04:14 +0700

    feat: Add exit button to HeaderChatSection component

--- src/components/HeaderChatSection.jsx
+++ src/components/HeaderChatSection.jsx
@@ -1,7 +1,12 @@
+import { LogOutIcon } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
 
 export function HeaderChatSection() {
+  function handleExitButton() {
+    window.location.href = "/";
+  }
   return (
     <>
       <div className="flex flex-col items-center justify-center text-center m-4">
@@ -14,6 +19,17 @@ export function HeaderChatSection() {
         </div>
         <div className="">
           <h1>Ada pertanyaan? Chat aja di sini!</h1>
+        </div>
+        <div>
+          <Button
+            size="sm"
+            variant="secondary"
+            className="w-32"
+            onClick={() => handleExitButton()}
+          >
+            <LogOutIcon size={24} className="mr-2" />
+            Exit
+          </Button>
         </div>
       </div>
       <Separator />
