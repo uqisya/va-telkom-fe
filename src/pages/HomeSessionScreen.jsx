92f7a224b795cbe014e1d6ac04035d268ca2a31b
Syauqi <syauqifrizman@gmail.com>
2024-08-14 14:20:10 +0700

    feat: Refactor HomeSessionScreen component to display chat session history as cards and add total chat viewer

--- src/pages/HomeSessionScreen.jsx
+++ src/pages/HomeSessionScreen.jsx
@@ -3,22 +3,22 @@ import axios from "axios";
 import { LogInIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 import { ChatScreen } from "./ChatScreen";
+import { CardItemChatSessionHistory } from "@/components/CardItemChatSessionHistory";
 
 export function HomeSessionScreen() {
   const [chatSessionsHistory, setChatSessionsHistory] = useState([]);
   const [error, setError] = useState(null);
   const [chatSession, setChatSession] = useState({});
 
-  const [selectedChatSession, setSelectedChatSession] = useState({});
-
   const [showChatScreen, setShowChatScreen] = useState(false);
+  const [totalChat, setTotalChat] = useState(0);
 
   useEffect(() => {
     async function getChatSessions() {
       try {
         const response = await axios.get("http://localhost:8000/api/chats");
         setChatSessionsHistory(response.data.data);
-        setSelectedChatSession(response.data.data[0]);
+        setTotalChat(response.data.data.length);
       } catch (error) {
         setError(error.response.data.message);
       }
@@ -31,7 +31,7 @@ export function HomeSessionScreen() {
     const selectedSession = chatSessionsHistory.filter(
       (session) => session.id === selectedId
     );
-    setSelectedChatSession(selectedSession[0]);
+    // setSelectedChatSession(selectedSession[0]);
   }
 
   async function startchatSession() {
@@ -44,8 +44,8 @@ export function HomeSessionScreen() {
     }
   }
 
-  function joinChatSession() {
-    setChatSession(selectedChatSession);
+  function joinChatSession(chatSession) {
+    setChatSession(chatSession);
     setShowChatScreen(true);
   }
 
@@ -55,31 +55,32 @@ export function HomeSessionScreen() {
 
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-screen">
-        <div className="mb-4">
-          <Button onClick={() => startchatSession()}>Create New Chat</Button>
-        </div>
-        <label>Select Session:</label>
-        <div className="my-4">
+      <div className="flex flex-col items-center h-screen m-4 mb-16">
+        <label className="font-semibold">Chat Session History</label>
+        <label className="text-sm text-gray-500">Total Chat: {totalChat}</label>
+        <div>
           {chatSessionsHistory.length === 0 ? (
             <p>No chat sessions available</p>
           ) : (
             <>
-              <select className="mr-4" onChange={handleSelectChange}>
-                {chatSessionsHistory.map((chatSession) => {
-                  return (
-                    <option key={chatSession.id} value={chatSession.id}>
-                      {chatSession.title}
-                    </option>
-                  );
-                })}
-              </select>
-              <Button size="icon" onClick={joinChatSession}>
-                <LogInIcon size="16" />
-              </Button>
+              {chatSessionsHistory.map((chatSession) => {
+                return (
+                  <>
+                    <CardItemChatSessionHistory
+                      chatSession={chatSession}
+                      setSelectedChatSession={joinChatSession}
+                    />
+                  </>
+                );
+              })}
             </>
           )}
         </div>
+      </div>
+      <div className="sticky bottom-0 p-4 bg-white flex items-center justify-center">
+        <Button className="w-full" onClick={() => startchatSession()}>
+          Create New Chat
+        </Button>
       </div>
     </>
   );
